2de06a933edb320dac617b5ac691032e5184601b
Miguelangel <silvermiguel96@gmail.com>
2019-04-12 12:09:44 -0500

    Directive

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,41 @@
 <template lang="pug">
   #app
-    h1 {{ msg }}
-    p {{ 1 + 1}}
-    p {{ 'Hola '+ 'msg'}}
-    p {{ person.name }}
-    p {{ person.name.toUpperCase()}}
-    p {{ JSON.stringify(person)}}
-    p {{ true ? true : false}}
+    p(v-show="mostrarDom") {{valor}}
+    p(v-if="mostrarDomIf") {{valorIf}}
+    p(v-else-if="mostrarDom") {{valor}}
+    p(v-else="mostrarDomIf") {{valorIf}}
+    ul
+      li(v-bind:key="i" v-for="i in array") {{i}}
+    ul
+      li(v-for="h in arrayObjetos" v-show="h.status") {{h.nombre}}
 </template>
-
 <script>
 export default {
   name: 'app',
   data () {
     return {
       msg: 'Hola mundo de vue!',
-
-      person: {
-        name: 'juan'
-      }
+      mostrarDom: true,
+      valor: 'Valor del Vshow',
+      mostrarDomIf: false,
+      valorIf: 'Valor del Vshowif',
+      // Collecion de objeto
+      array: [1, 2, 3, 4, 5],
+      arrayObjetos: [
+        { nombre: '1 for',
+          status: true
+        },
+        { nombre: '2 for',
+          status: false
+        },
+        { nombre: '3 for',
+          status: true
+        }
+      ]
     }
   }
 }
 </script>
-
 <style lang="scss">
 @import './scss/main.scss'
 </style>
