a85c83534502b8b162de10fcb27ff3fe518b1734
Miguelangel <silvermiguel96@gmail.com>
2019-04-14 18:38:42 -0500

    Data binding

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,17 @@
 <template lang="pug">
   #app
-    p(v-show="mostrarDom") {{valor}}
-    p(v-if="mostrarDomIf") {{valorIf}}
-    p(v-else-if="mostrarDom") {{valor}}
-    p(v-else="mostrarDomIf") {{valorIf}}
-    ul
-      li(v-bind:key="i" v-for="i in array") {{i}}
-    ul
-      li(v-for="h in arrayObjetos" v-show="h.status") {{h.nombre}}
+    input(v-model="name")
+    p {{name}}
+
+    a(v-bind:href="url") Enviar_
 </template>
 <script>
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Hola mundo de vue!',
-      mostrarDom: true,
-      valor: 'Valor del Vshow',
-      mostrarDomIf: false,
-      valorIf: 'Valor del Vshowif',
-      // Collecion de objeto
-      array: [1, 2, 3, 4, 5],
-      arrayObjetos: [
-        { nombre: '1 for',
-          status: true
-        },
-        { nombre: '2 for',
-          status: false
-        },
-        { nombre: '3 for',
-          status: true
-        }
-      ]
+      name: '',
+      url: 'https://www.google.com'
     }
   }
 }
