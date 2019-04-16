05a1513f71c3b62048ca86a62e8d2e754fb484b7
Miguelangel <silvermiguel96@gmail.com>
2019-04-15 21:22:55 -0500

    computed properties

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template lang="pug">
   #app
+    .label Nombre
     input(v-model="name")
-    p {{name}}
-
-    a(v-bind:href="url") Enviar_
+    .label Apellido
+    input(v-model="lastName")
+    p {{fullName}}
+    .label Fecha de nacimiento
+    input(type="date" v-model="birthday")
+    p {{calcYear}}
 </template>
 <script>
 export default {
@@ -11,7 +15,22 @@ export default {
   data () {
     return {
       name: '',
-      url: 'https://www.google.com'
+      lastName: '',
+      birthday: ''
+    }
+  },
+  computed: {
+    fullName () {
+      return `${this.name} ${this.lastName}`
+    },
+    calcYear () {
+      if (this.birthday) {
+        let birthday = new Date(this.birthday)
+        let today = new Date()
+        let years = today.getFullYear() - birthday.getFullYear()
+        return years
+      }
+      return null
     }
   }
 }
