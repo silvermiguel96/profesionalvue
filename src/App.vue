0d1db55e271cf1892278b310bcc541ebc2314d7e
Miguelangel <silvermiguel96@gmail.com>
2019-04-16 21:10:03 -0500

    event

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,8 @@
   #app
     .label Nombre
     input(v-model="name")
-    .label Apellido
-    input(v-model="lastName")
+    button(v-on:click="compilarName") Enviar
     p {{fullName}}
-    .label Fecha de nacimiento
-    input(type="date" v-model="birthday")
-    p {{calcYear}}
 </template>
 <script>
 export default {
@@ -15,28 +11,12 @@ export default {
   data () {
     return {
       name: '',
-      lastName: '',
-      birthday: '',
-      years: ''
+      fullName: ''
     }
   },
-  computed: {
-    fullName () {
-      return `${this.name} ${this.lastName}`
-    },
-    calcYear () {
-      if (this.birthday) {
-        let birthday = new Date(this.birthday)
-        let today = new Date()
-        let years = today.getFullYear() - birthday.getFullYear()
-        return years
-      }
-      return null
-    }
-  },
-  watch: {
-    calcYear (newYear, oldyear) {
-      console.log(newYear, newYear)
+  methods: {
+    compilarName () {
+      this.fullName = this.name.split(' ').join('-')
     }
   }
 }
